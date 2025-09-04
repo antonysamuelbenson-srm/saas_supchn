f6a7c120869c38cbbd98c4750c1e46069b7ff53f
ancybjohn02 <ancybjohn26@gmail.com>
2025-09-04 09:54:02 +0530

    csv update

--- backend/app/routes/upload.py
+++ backend/app/routes/upload.py
@@ -22,7 +22,7 @@ REQUIRED_COLS = {
     ],
     "inventory": ["snapshot_date", "store_code", "sku", "qty", "product_name"],
     "forecast":  ["forecast_date", "store_code", "sku", "forecast_qty"],
-    "total_store_data": ["node_name", "sku", "safety_stock_level", "reorder_level"],
+    "total_store_data": ["store_code", "sku", "safety_stock_level", "reorder_level"],
     "transfer_cost_data": ["start_location", "end_location", "transfer_cost"],
     "warehouse_max_data": ["store_id", "warehouse_name", "max_capacity"]
 }
