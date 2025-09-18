6fbfa1f948ed6241c9c1e6c0f0736f68ecc08bf1
ancybjohn02 <ancybjohn26@gmail.com>
2025-09-18 13:35:29 +0530

    added forecast related file

---
+++ backend/app/models/forecast_log.py
@@ -0,0 +1,30 @@
+from app import db
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+
+class ForecastLog(db.Model):
+    __tablename__ = "forecast_log"
+
+    # id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True),primary_key=True)
+    run_time = db.Column(db.DateTime, nullable=False)
+    n_days = db.Column(db.Integer, nullable=False)
+    run_started_at = db.Column(db.DateTime, nullable=True)
+    run_completed_at = db.Column(db.DateTime, nullable=True)
+    status = db.Column(db.String(50), nullable=True)
+
+    def __repr__(self):
+        return (
+            f"<ForecastLog id={self.id}"
+            f"run_time={self.run_time} status={self.status}>"
+        )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "run_time": self.run_time.strftime("%Y-%m-%d %H:%M:%S") if self.run_time else None,
+            "n_days": self.n_days,
+            "run_started_at": self.run_started_at.strftime("%Y-%m-%d %H:%M:%S") if self.run_started_at else None,
+            "run_completed_at": self.run_completed_at.strftime("%Y-%m-%d %H:%M:%S") if self.run_completed_at else None,
+            "status": self.status
+        }
